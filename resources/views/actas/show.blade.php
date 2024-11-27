1aaa2d71525552055900816d3c7b9055d3f1cb4a
Alexis Palacios <oscaralexispalacios9@gmail.com>
2024-11-27 13:02:09 -0600

    show acuerdos

--- resources/views/actas/show.blade.php
+++ resources/views/actas/show.blade.php
@@ -9,11 +9,17 @@
 @section('content')
     <div class="card">
         <div class="card-body">
-            <h5>Libro: {{ $acta->id_libros }}</h5>
-            <h5>Personal: {{ $acta->id_Personal }}</h5>
-            <h5>Fecha: {{ $acta->fecha }}</h5>
-            <h5>Descripción:</h5>
-            <p>{{ $acta->descripcion }}</p>
+            <h5><strong>Id Actas:</strong> {{ $acta->id_Actas }}</h5>
+            <h5><strong>Libro:</strong> {{ $acta->id_libros }}</h5>
+            <h5><strong>Fecha:</strong> {{ $acta->fecha }}</h5>
+            <h5><strong>Correlativo:</strong> {{ $acta->correlativo }}</h5>
+            <h5><strong>Contenido de Elaboración:</strong></h5>
+            <p>{{ $acta->contenido_elaboracion }}</p>
+            <h5><strong>Presentes:</strong> {{ $acta->presentes }}</h5>
+            <h5><strong>Ausentes:</strong> {{ $acta->ausentes }}</h5>
+            <h5><strong>Tipo de Sesión:</strong> {{ $acta->tipo_sesion }}</h5>
+            <h5><strong>Contenido Acta:</strong> {{ $acta->motivo_ausencia }}</h5>
+            
         </div>
         <a href="{{ route('actas.index') }}" class="btn btn-secondary">Volver</a>
     </div>
